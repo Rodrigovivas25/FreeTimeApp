8ab484006e0984c2c0139d20920673f7950bd323
Rodrigovivas25 <rodrigo.vivas.25@hotmail.com>
2019-07-23 03:57:14 -0500

    Creación del proyecto

---
+++ ProyectoFreeTimeApp/ProyectoFreeTimeApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ProyectoFreeTimeApp
+//
+//  Created by Rodrigo Vivas on 7/23/19.
+//  Copyright © 2019 Rodrigo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
