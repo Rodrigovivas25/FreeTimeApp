5f69e9c53f361aa7a3c337284bbb2e8352cc3d84
NoemiRod <miroddd@outlook.com>
2019-07-27 14:06:47 -0500

    Cambios en la interfaz ininicial y creacion del VC de los eventos

--- ProyectoFreeTimeApp/ProyectoFreeTimeApp/ViewController.swift
+++ ProyectoFreeTimeApp/ProyectoFreeTimeApp/ViewController.swift
@@ -9,16 +9,27 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
 
     @IBOutlet weak var backgroundImage: UIImageView!
     
+    @IBOutlet weak var scheduledEvents: UIButton!
+    @IBOutlet weak var searchEvents: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        backgroundImage.image = UIImage(named: "lightblue")!
-        self.view.sendSubviewToBack(backgroundImage)
-        // Do any additional setup after loading the view.
+        
+        scheduledEvents.layer.cornerRadius = 10
+        searchEvents.layer.cornerRadius = 10
+        
+        
+        
+        let backgroundImage = UIImageView(frame: UIScreen.main.bounds)
+        backgroundImage.image = UIImage(named: "unam")
+        backgroundImage.contentMode = UIView.ContentMode.scaleAspectFill
+        self.view.insertSubview(backgroundImage, at: 0)
+        
+        self.title = "Free Time App"
+
     }
-
-    
 }
-
